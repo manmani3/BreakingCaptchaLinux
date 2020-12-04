3e4399d072b43a527315c81b9f9a841d94e834b8
jeonghyeonYun <manmani3@github.com>
2020-12-05 00:50:16 +0900

    return image inference information

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ import socket
 import cv2
 import numpy
 
-
 # socket recive buffer
 def recvall(sock, count):
     buf = b''
@@ -16,79 +15,71 @@ def recvall(sock, count):
 
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 9999
+TCP_PORT = 1234
 
 # open tcp socket and wait
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', TCP_PORT))
-s.listen(True)
 
-print('9999port waiting')
+while True:
 
-conn, addr = s.accept()
+    s.listen(True)
+    conn, addr = s.accept()
 
-# recive 16bit
-length = recvall(conn, 16)
-stringData = recvall(conn, int(length))
-data = numpy.frombuffer(stringData, dtype='uint8')
-s.close()
-decimg=cv2.imdecode(data, 1)
-cv2.imwrite('test.png', decimg)
+    print("accpet")
+    print(addr)
 
-# Image Test code
-"""
-cv2.imshow('SERVER', decimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
-"""
+    # recive 16bit
+    length = recvall(conn, 16)
+    stringData = recvall(conn, int(length))
+    data = numpy.frombuffer(stringData, dtype='uint8')
 
-from detectron2.config import get_cfg
-from detectron2.engine import DefaultPredictor
-from detectron2 import model_zoo
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.DEVICE='cpu'
-cfg.MODEL.WEIGHTS = "./model_final_1204.pth"
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 15
+    decimg=cv2.imdecode(data, 1)
+    cv2.imwrite('222.png', decimg)
 
-predictor = DefaultPredictor(cfg)
-outputs = predictor(decimg)
+    from detectron2.config import get_cfg
+    from detectron2.engine import DefaultPredictor
+    from detectron2 import model_zoo
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.DEVICE='cpu'
+    cfg.MODEL.WEIGHTS = "./model_final_1204.pth"
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 15
 
-print('-----------------------------------------------------')
-print(outputs["instances"])
-print('-----------------------------------------------------')
-print('### stop running server ###')
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(decimg)
 
-# get inference information
-instances = outputs['instances']
-titleInstance = instances[instances.pred_classes >= 8]
-if (len(titleInstance) == 0):
-    raise Exception('There is no title text')
+    print('-----------------------------------------------------')
+    print(outputs["instances"])
+    print('-----------------------------------------------------')
 
-titleClass = titleInstance.pred_classes.numpy()[0]
-titleBox = titleInstance.pred_boxes.tensor.numpu()
-titleLeftBottomOffest = (titleBox[0][0], titleBox[0][3])
+    # get inference information
+    instances = outputs['instances']
+    """
+    titleInstance = instances[instances.pred_classes >= 8]
+    if (len(titleInstance) == 0):
+        raise Exception('There is no title text')
 
-targetInstances = instances[instances.pred_classed == (titleClass - 8)]
-targetInstancesMasks = targetInstances.pred_masks.numpy()
+    titleClass = titleInstance.pred_classes.numpy()[0]
+    titleBox = titleInstance.pred_boxes.tensor.numpu()
+    titleLeftBottomOffest = (titleBox[0][0], titleBox[0][3])
+    """
+    targetInstances = instances[instances.pred_classes < 8] # == (titleClass - 8)]
+    targetInstancesMasks = targetInstances.pred_masks.numpy()
 
-# tuple format. ex) (23.145, 35.222)
-result['titleLeftBottomOffset'] = titleLeftBottomOffset
-# 3D array. each 2D array is mask for each one instance
-result['targetInstancesMasks'] = targetInstancesMasks
+    result = {}
+    # tuple format. ex) (23.145, 35.222)
+    # result['titleLeftBottomOffset'] = titleLeftBottomOffset
+    # 3D array. each 2D array is mask for each one instance
+    result['targetInstancesMasks'] = targetInstancesMasks
+    print("result: ")
+    print(result)
+    # return inference result to client
 
-# return inference result to client
-import pickle
-HOST = '218.209.197.49'
-PORT = 9999
+    import pickle
+    resultData = pickle.dumps(result)
 
-print('connecting to ' + HOST + ', ' + str(PORT))
-s = socket.socket()
-s.connect((HOST, PORT))
-
-resultData = pickle.dumps(result)
-s.send(resultData)
-
-s.close()
-print('Return data to client')
+    conn.send(str(len(resultData)).ljust(16).encode('utf-8'))
+    conn.send(resultData)
+    conn.close()
